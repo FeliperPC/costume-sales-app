106a8437870bb714e9c2ab4067d2bac68748d1bb
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-02-15 13:22:34 -0300

    feat : hero section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Hero } from "@/components/Hero";
 import Navbar from "@/components/NavBar";
 import Header from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="pt-20 bg-zinc-950 text-white min-h-screen">
+      {/* Hero Section */}
+      <Hero />
+    </div>
   );
 }
